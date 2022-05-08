b4412084b530efe18a120fd21c46f0c02f770b66
okaryo <okaryotr@gmail.com>
2022-05-08 23:28:46 +0900

    [add] mock file

---
+++ test/mock/mock.dart
@@ -0,0 +1,9 @@
+import 'package:long_time_no_notification/src/notification_repository.dart';
+import 'package:long_time_no_notification/src/notification_shared_preferences.dart';
+import 'package:mockito/annotations.dart';
+
+@GenerateMocks([
+  NotificationSharedPreferences,
+  NotificationRepository,
+])
+void main() {}

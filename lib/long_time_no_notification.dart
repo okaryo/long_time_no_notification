aa89c5e18577cc9b510070a2b44d1989ff66a4a1
okaryo <okaryotr@gmail.com>
2022-05-08 23:30:34 +0900

    [modify] export file

--- lib/long_time_no_notification.dart
+++ lib/long_time_no_notification.dart
@@ -1,7 +1,3 @@
 library long_time_no_notification;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/long_time_no_notification.dart';
